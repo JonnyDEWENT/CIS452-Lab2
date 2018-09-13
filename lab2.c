c6ff60937187fbffef6a9a530e6ffc943509d0f7
Cheng Li <32421669+lichal@users.noreply.github.com>
2018-09-13 13:44:59 -0400

    update indentation

--- lab2.c
+++ lab2.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/time.h>
+#include <sys/wait.h>
 #include <sys/resource.h>
 /***********************************************************************
  * This program simulate a simple shell, it will take in shell command
@@ -14,6 +15,7 @@
  * @author Li, Cheng
  * @version September 12, 2018
  **********************************************************************/
+
 int main(void) {
     /* An array of pointers pointing to user enter arguments. */
     char *argv[64];
@@ -22,13 +24,12 @@ int main(void) {
     char buffer[1000];
     
     /* Parent pid and child status. */
-    pid_t  pid, child;
+    pid_t  pid;
     int status;
     
     /* Variable declared to get resource usage. */
     struct rusage* pusage = malloc(sizeof(struct rusage*));
-    int* parentpid = malloc(sizeof(int*));
-    printf("\nParent PID: %ld\n", (long)getpid());
+    int* childpid = malloc(sizeof(int*));
     
     while(1){
         printf("[Shell] -> ");
@@ -41,6 +42,9 @@ int main(void) {
         
         /* Exiting program when user types quit */
         if(!strncmp("quit",buffer,4)){
+			/* Free malloc */
+            free(pusage);
+			free(childpid);
             exit(1);
         }
         /* Tokenizing the user input */
@@ -57,9 +61,9 @@ int main(void) {
             
             /* getting process resource statistics and printing */
             getrusage(RUSAGE_SELF, pusage);
-            *parentpid = getpid();
-            printf("\n\nPROCESS PID: %i\nUSER CPU TIME: %ld.%d\nINVOLUNTARY CONTEXT SWITCHES: %ld\n\n\n", *parentpid, pusage->ru_utime.tv_sec,pusage->ru_utime.tv_usec, pusage->ru_nivcsw);
-            
+            *childpid = getpid();
+            printf("\n\nPROCESS PID: %i\nUSER CPU TIME: %ld.%ld\nINVOLUNTARY CONTEXT SWITCHES: %ld\n\n\n", *childpid, pusage->ru_utime.tv_sec,pusage->ru_utime.tv_usec, pusage->ru_nivcsw);
+			
             /* Execute command line input */
             if (execvp(*argv, argv) < 0) {
                 perror("exec failed");
@@ -68,7 +72,7 @@ int main(void) {
         }
         
         else{                               /* The parent waits for child to finish. */
-            child = wait(&status);
+            wait(&status);
         }
     }
     return 1;
