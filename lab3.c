edf261053b14d2372f5a04877d29597eafeef1e6
Cheng Li <32421669+lichal@users.noreply.github.com>
2018-09-25 18:55:48 -0400

    Recovering lab 3

--- lab3.c
+++ lab3.c
@@ -1,57 +1,74 @@
-#include <pthread.h>
-#include <stdio.h>
-#include <errno.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <string.h>
+#include <signal.h>
+#include <stdio.h>
 
-//using namespace std;
-void* fileGet(void* arg);
-void sigHandler (int sigNum);
+/***********************************************************************
+ * This program is a simple Communicating Processes, it will communicate
+ * between the parent and child regarding signals send, and quit
+ * elegantly when ^C is send.
+ *
+ * @author Dewent, Jon
+ * @author Li, Cheng
+ * @version September 19, 2018
+ **********************************************************************/
 
-int filesAccessed = 0;
-char *filename;
+void sigHandler (int);
+pid_t *childPid;
 
 int main()
 {
-    filename = malloc(sizeof(char)*100);
-    pthread_t thread;
-    signal (SIGINT, sigHandler);
-    while (fgets(filename, 100, stdin) != NULL) {
-        int status;
-        
-        if ((status = pthread_create (&thread, NULL,  fileGet, filename) != 0)) {
-            fprintf (stderr, "thread create error %d: %s\n", status, strerror(status));
-            exit (1);
-        }
-
-        filesAccessed++;
+    childPid = malloc(sizeof(int*));
+    int parentID = getpid();
+    int pid;
+    if((pid = fork()) < 0) {
+        perror ("fork error");
+        exit(1);
     }
+
+    // Child process handling
+    if (!pid){
+        while(1){
+            int randomTime = (rand() % 5)+1; /* Wait a time of 1-5 secs*/
+            sleep(randomTime);
+            int randomSignal = rand() % 2;  /* decide to send which signal */
+            if(randomSignal)
+                kill(parentID, SIGUSR2);
+            else
+                kill(parentID, SIGUSR1);
+        }
+    }
+
+    // Parent process handling
+    printf("Spawned child PID# %d\n",pid);
+    *childPid = pid;
+    while(1) {
+        printf("waiting...\t");
+        fflush(stdout);
+        signal (SIGINT, sigHandler);        /* Signal handlers */
+        signal (SIGUSR1, sigHandler);
+        signal (SIGUSR2, sigHandler);
+        pause();
+    }
+
+    return 0;
 }
 
 // This function handles signal SIGINT and terminates the processes
 void sigHandler (int sigNum)
 {
-    if(sigNum == SIGINT){
-        printf (" received.\nNumber Files Accessed: %d\n", filesAccessed);
-        printf("Shutting down...\n");
-        free(filename);
-        // Prepare to shutdown.
+    if(sigNum == SIGUSR1){
+        printf ("received a SIGUSR1 signal.\n");
+    }
+    else if(sigNum == SIGUSR2){
+        printf ("received a SIGUSR2 signal.\n");
+    }
+    else{
+        printf (" received. Thats it, I'm shutting you down...\n");
+        // this is where shutdown code would be inserted
         sleep (1);
+        kill(*childPid, SIGINT);
+        free(childPid);
         exit(0);
     }
-}
-
-void* fileGet(void* arg){
-    int wait = rand() % 5;
-    if(wait > 0){
-        sleep(1);
-    }
-    else {
-        int waitTime = rand() % 4 + 7;
-        sleep(waitTime);
-    }
-    printf("File Accessed: %s \n", arg);
-    return arg;
-    
 }
