e382ee2e253761ab87bbc151d242d1c5cbe97380
Cheng Li <32421669+lichal@users.noreply.github.com>
2018-09-25 18:55:07 -0400

    Uploaded to the wrong file, upload again

--- lab4.c
+++ lab4.c
@@ -1,36 +1,45 @@
-
-
 #include <pthread.h>
 #include <stdio.h>
 #include <errno.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <string.h> 
+#include <string.h>
 
 //using namespace std;
 void* fileGet(void* arg);
+void sigHandler (int sigNum);
+
+int filesAccessed = 0;
+char *filename;
 
 int main()
 {
-
-int filesAccessed = 0;
-char * filename;
-void* result1;
-
- pthread_t thread;
-
+    filename = malloc(sizeof(char)*100);
+    pthread_t thread;
+    signal (SIGINT, sigHandler);
     while (fgets(filename, 100, stdin) != NULL) {
+        int status;
+        
+        if ((status = pthread_create (&thread, NULL,  fileGet, filename) != 0)) {
+            fprintf (stderr, "thread create error %d: %s\n", status, strerror(status));
+            exit (1);
+        }
 
-    int status;
-
-    if ((status = pthread_create (&thread, NULL,  fileGet, filename) != 0)) {
-        fprintf (stderr, "thread create error %d: %s\n", status, strerror(status));
-        exit (1);
+        filesAccessed++;
     }
-    
-    filesAccessed++;
-    
-    } 
+}
+
+// This function handles signal SIGINT and terminates the processes
+void sigHandler (int sigNum)
+{
+    if(sigNum == SIGINT){
+        printf (" received.\nNumber Files Accessed: %d\n", filesAccessed);
+        printf("Shutting down...\n");
+        free(filename);
+        // Prepare to shutdown.
+        sleep (1);
+        exit(0);
+    }
 }
 
 void* fileGet(void* arg){
@@ -39,11 +48,10 @@ void* fileGet(void* arg){
         sleep(1);
     }
     else {
-        int wait2 = rand() % 4 + 10;
-        sleep(wait2);
-    } 
+        int waitTime = rand() % 4 + 7;
+        sleep(waitTime);
+    }
     printf("File Accessed: %s \n", arg);
-    //free(arg);
-        return arg;
-        
+    return arg;
+    
 }
